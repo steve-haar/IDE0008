c277c7c79ddce487a32a33f2a93a41ae54328c63
Steve Haar <info@stevehaar.com>
2019-07-16 07:34:13 -0500

    initial commit

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IDE0008
+{
+    public class Class1
+    {
+        public static void Test()
+        {
+            var date = DateTime.UtcNow;
+            var color = Color.Red;
+            var date2 = new DateTime(1);
+            var color2 = (Color)1;
+
+            Console.WriteLine($"{date}{date2}{color}{color2}");
+        }
+    }
+
+    internal enum Color
+    {
+        Red,
+        Green,
+        Blue
+    }
+}
